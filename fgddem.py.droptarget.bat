962bcd1d2a7133e35014e97088f5ee2741f68ecd
Minoru Akagi <akaginch@gmail.com>
2025-12-16 10:13:37 +0900

    clean up fgddem.py.droptarget.bat

--- fgddem.py.droptarget.bat
+++ fgddem.py.droptarget.bat
@@ -2,14 +2,6 @@
 SET OSGEO4W_ROOT=C:\OSGeo4W
 call %OSGEO4W_ROOT%\bin\o4w_env.bat
 
-rem Python 3.x (need to check python version)
-SET PYTHONHOME=%OSGEO4W_ROOT%\apps\Python312
-PATH %PYTHONHOME%\Scripts;%PATH%
-
-rem Qt 5
-rem PATH %OSGEO4W_ROOT%\apps\Qt5\bin;%PATH%
-rem set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins
-
 echo on
 python3 "%~dp0fgddem.py" %*
 
